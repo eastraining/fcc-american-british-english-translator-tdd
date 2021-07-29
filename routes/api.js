0b4832211921a9679e9609df7043e1c0e8660ba2
eastraining <21341281+eastraining@users.noreply.github.com>
2021-07-29 22:17:35 +0800

    completed functional testing

--- routes/api.js
+++ routes/api.js
@@ -5,9 +5,26 @@ const Translator = require('../components/translator.js');
 module.exports = function (app) {
   
   const translator = new Translator();
+  const createErr = msg => {
+    return { error: msg };
+  }
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      const sendErr = msg => res.json(createErr(msg));
+
+      if (!req.body.hasOwnProperty('text') || !req.body.hasOwnProperty('locale')) {
+        sendErr('Required field(s) missing');
+        return;
+      } else if (!req.body.text) {
+        sendErr('No text to translate');
+        return;
+      }
+
+      let textObj = {
+        text: req.body.text,
+        locale: req.body.locale
+      }
+      res.json(translator.translate(textObj));
     });
 };
