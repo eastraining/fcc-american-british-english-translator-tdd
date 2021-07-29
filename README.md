4d63234e9e59bc4ace416897419ebd6af55cd410
Yu Dong Hee <21341281+eastraining@users.noreply.github.com>
2021-07-29 03:04:39 +0000

    initial set up

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # [Quality Assurance Projects - American / British Translator](https://www.freecodecamp.org/learn/quality-assurance/quality-assurance-projects/american-british-translator)
+My implementation of this project. Try it out on Repl.it:
+
+[![Run on Repl.it](https://repl.it/badge/github/eastraining/fcc-american-british-english-translator-tdd)](https://repl.it/github/eastraining/fcc-american-british-english-translator-tdd)
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,7 +2,14 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Translator = require('../components/translator.js');
+const translator = new Translator();
 
 suite('Unit Tests', () => {
-
+  suite('American to British English tests', () => {
+    // #1
+    test('Mangoes are my favorite fruit.', () => {
+      assert.equal(translator.aToB('Mangoes are my favorite fruit'),
+      'Mangoes are my favourite fruit.');
+    });
+  })
 });
