cb928120dd01d51ef9fcfd67a310eaf8fbefb124
eastraining <21341281+eastraining@users.noreply.github.com>
2021-07-29 15:19:53 +0800

    drafted unit tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -69,5 +69,131 @@ suite("Unit Tests", () => {
         ["Dr Grosh will see you now.", "Dr"]
       );
     });
-  })
+    // #10
+    test("Lunch is at 12:15 today.", () => {
+      assert.sameOrderedMembers(
+        translator.aToB("Lunch is at 12:15 today."),
+        ["Lunch is at 12.15 today.", "12.15"]
+      );
+    });
+  });
+  
+  suite("British to American English tests", () => {
+    // #11
+    test("We watched the footie match for a while.", () => {
+      assert.sameOrderedMembers(
+        translator.bToA("We watched the footie match for a while."),
+        ["We watched the soccer match for a while.", "soccer"]
+      );
+    });
+    // #12
+    test("Paracetamol takes up to an hour to work.", () => {
+      assert.sameOrderedMembers(
+        translator.bToA("Paracetamol takes up to an hour to work."),
+        ["Tylenol takes up to an hour to work.", "Tylenol"]
+      );
+    });
+    // #13
+    test("First, caramelise the onions.", () => {
+      assert.sameOrderedMembers(
+        translator.bToA("First, caramelise the onions."),
+        ["First, caramelize the onions.", "Tylenol"]
+      );
+    });
+    // #14
+    test("I spent the bank holiday at the funfair.", () => {
+      assert.sameOrderedMembers(
+        translator.bToA("I spent the bank holiday at the funfair."),
+        ["I spent the public holiday at the carnival.", 
+        "public holiday", "carnival"]
+      );
+    });
+    // #15
+    test("I had a bicky then went to the chippy.", () => {
+      assert.sameOrderedMembers(
+        translator.bToA("I had a bicky then went to the chippy."),
+        ["I had a cookie then went to the chippy.", 
+        "cookie", "fish-and-chip shop"]
+      );
+    });
+    // #16
+    test("I've just got bits and bobs in my bum bag.", () => {
+      assert.sameOrderedMembers(
+        translator.bToA("I've just got bits and bobs in my bum bag."),
+        ["I've just got odds and ends in my fanny pack.", 
+        "odds and ends", "fanny pack"]
+      );
+    });
+    // #17
+    test("The car boot sale at Boxted Airfield was called off.", () => {
+      assert.sameOrderedMembers(
+        translator.bToA("The car boot sale at Boxted Airfield was called off."),
+        ["The swap meet at Boxted Airfield was called off.", "swap meet"]
+      );
+    });
+    // #18
+    test("Have you met Mrs Kalyani?", () => {
+      assert.sameOrderedMembers(
+        translator.bToA("Have you met Mrs Kalyani?"),
+        ["Have you met Mrs. Kalyani?", "Mrs."]
+      );
+    });
+    // #19
+    test("Prof Joyner of King's College, London.", () => {
+      assert.sameOrderedMembers(
+        translator.bToA("Prof Joyner of King's College, London."),
+        ["Prof. Joyner of King's College, London.", "Prof."]
+      );
+    });
+    // #20
+    test("Tea time is usually around 4 or 4.30.", () => {
+      assert.sameOrderedMembers(
+        translator.bToA("Tea time is usually around 4 or 4.30."),
+        ["Tea time is usually around 4 or 4:30.", "4:30"]
+      );
+    });
+  });
+
+  suite("Highlight translation", () => {
+    // #21
+    test("Mangoes are my favorite fruit.", () => {
+      assert.equal(
+        translator.translate({
+          text: "Mangoes are my favorite fruit.", 
+          locale: "american-to-british"
+        }),
+        "Mangoes are my <span class='highlight'>favourite</span> fruit."
+      );
+    });
+    // #22
+    test("I ate yogurt for breakfast.", () => {
+      assert.equal(
+        translator.translate({
+          text: "I ate yogurt for breakfast.",
+          locale: "american-to-british"
+        }),
+        "I ate <span class='highlight'>yoghurt</span<> for breakfast."
+      );
+    });
+    // #23
+    test("We watched the footie match for a while.", () => {
+      assert.equal(
+        translator.translate({
+          text: "We watched the footie match for a while.",
+          locale: "british-to-american"
+        }),
+        "We watched the <span class='highlight'>soccer</span> match for a while."
+      );
+    });
+    // #24
+    test("Paracetamol takes up to an hour to work.", () => {
+      assert.equal(
+        translator.translate({
+          text: "Paracetamol takes up to an hour to work.",
+          locale: "british-to-american"
+        }),
+        "<span class='highlight'>Tylenol</span> takes up to an hour to work."
+      );
+    });
+  });
 });
